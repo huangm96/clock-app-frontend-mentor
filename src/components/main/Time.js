131ac266536dbad11d9ea782d74e5a0018776dbe
Min-Huang <minh.huang96@gmail.com>
2021-01-16 12:28:55 -0800

    working on time

--- src/components/main/Time.js
+++ src/components/main/Time.js
@@ -1,29 +1,40 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import Axios from "axios";
 
 import { initialState, reducer } from "../utility/Reducer";
 import "./Time.css";
 
 function Time() {
-    const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    getTime()
+  },[])
+  const getTime = () => {
+    Axios.get("https://freegeoip.app/json/", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch();
+  };
 
-   
-    return (
-      <div className="Time">
-        <div
-          className="Greeting"
-                
-        >
-          <p>Good morning</p>
-        </div>
-        <div className="Current-time">
-          <p>11:37</p>
-        </div>
-        <div className="Location">
-          <p>LonDon</p>
-        </div>
+  return (
+    <div className="Time">
+      <div className="Greeting">
+        <p>Good morning</p>
       </div>
-    );
+      <div className="Current-time">
+        <p>11:37</p>
+      </div>
+      <div className="Location">
+        <p>LonDon</p>
+      </div>
+    </div>
+  );
 }
 
 export default Time;
